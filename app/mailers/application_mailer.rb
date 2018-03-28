3eac309bb042d4dfdac2e85b733a471742e121d2
Shane Kelly <home@Shanes-MacBook-Pro.local>
2018-03-28 11:16:18 +0100

    re committing

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'test@shanekelly.me'
+  layout 'mailer'
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+  #get 'store/index'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+end
